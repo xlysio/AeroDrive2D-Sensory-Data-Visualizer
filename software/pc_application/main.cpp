3d09c99fc8de440030bf4cf5d77bc49d3a8e1d8e
Łukasz Lysek <lukasz.lysek567@gmail.com>
2026-04-07 21:02:29 +0200

    added doxygen config, project's logo, doxygen comments to main.cpp and removed mainwindow.ui

--- software/pc_application/main.cpp
+++ software/pc_application/main.cpp
@@ -1,7 +1,17 @@
+/*
+ * @file main.cpp
+ * @brief Creates the QApplication instance and displays the main window.
+ */
+
 #include "mainwindow.h"
 
 #include <QApplication>
 
+/* @brief Application's main function
+ * @param argc Number of arguments.
+ * @param argv Array of type string arguments.
+ * @return Exit code returned by Qt.
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
